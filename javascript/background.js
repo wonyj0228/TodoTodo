67ed77933e93cecbe5a35a781d8e9e2e277ee120
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-09-15 06:41:57 +0900

    layout, bg img setting
    #1
    - layout을 잡긴했으나 내부 contents 넣으면서 수정될 필요는 있음
    - bg change기능까지 구현 완료

---
+++ javascript/background.js
@@ -0,0 +1,20 @@
+const bgImg = document.querySelector('.background-img');
+const chgTheme = document.querySelector('.chg-theme');
+const imgArray = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+];
+
+function makeRandomImg() {
+  const img = imgArray[Math.floor(Math.random() * imgArray.length)];
+  bgImg.src = '/img/' + img;
+}
+
+makeRandomImg();
+
+chgTheme.addEventListener('click', makeRandomImg);
