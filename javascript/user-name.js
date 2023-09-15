e1d0514895e53342f65a3b629432365fa1d266df
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-09-16 03:01:02 +0900

    user name 저장
    #2
    - username loacal storage 저장
    - 조건별 display
    - main box 퍼블리싱

---
+++ javascript/user-name.js
@@ -0,0 +1,37 @@
+const submitForm = document.getElementById('main-box__userName');
+const submitInput = submitForm.querySelector('input');
+const todoDiv = document.querySelector('.main-box__todo');
+const todoUsername = todoDiv.querySelector('h1');
+
+function submitUsernameDisplay() {
+  todoDiv.classList.add('display-none');
+  submitForm.classList.remove('display-none');
+}
+
+function todoDisplay(userName) {
+  submitForm.classList.add('display-none');
+  todoDiv.classList.remove('display-none');
+  todoUsername.innerText = `Hello ${userName}`;
+}
+
+function saveUsername(userName) {
+  localStorage.setItem('userName', userName);
+
+  todoDisplay(userName);
+}
+
+function submitUsername(event) {
+  event.preventDefault();
+
+  const userName = submitInput.value;
+  saveUsername(userName);
+}
+
+const localUsername = localStorage.getItem('userName');
+
+if (!localUsername) {
+  submitUsernameDisplay();
+  submitForm.addEventListener('submit', submitUsername);
+} else {
+  todoDisplay(localUsername);
+}
